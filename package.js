576d15952e54cbbbb402cfb85bfd3a03fffaa57b
Rami Jarrar <ramijarrar@outlook.com>
2015-06-19 22:50:54 +0400

    Add basic package description

---
+++ package.js
@@ -0,0 +1,6 @@
+Package.describe({
+    name: 'gliese-technology:picnic',
+    version: '0.0.1',
+    summary: 'Picnic CSS library wrapped for meteor',
+    git: 'https://github.com/gliese-technology/picnic'
+});
